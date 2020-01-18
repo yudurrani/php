b739aa5c50182d65ea2acb5c2809269517ec8829
Yasir Durrani <yudurrani@gmail.com>
2020-01-17 19:50:06 -0500

    Add files via upload

---
+++ site.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title></title>
+	</head>
+	
+	<body>
+	
+		<?php
+			
+			$characterName = " Aizah ";
+			$age = 19;
+			echo "My name is $characterName <br>"; 
+			echo "I am going to rock by $age <br>";
+			echo "Will i be a hit or miss as $characterName by $age <br>";
+		?>
+	
+	</body>
+</html>
